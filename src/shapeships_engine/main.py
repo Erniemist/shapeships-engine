9d809612d1808f34953eb362310b35293fb3f768
mattkershaw <mattkershaw@capuk.org>
2025-09-10 14:22:26 +0100

    Create requests

--- src/shapeships_engine/main.py
+++ src/shapeships_engine/main.py
@@ -1,7 +1,9 @@
 import random
+from copy import deepcopy
 from dataclasses import dataclass
 from enum import Enum, StrEnum, auto
 from functools import reduce
+from typing import Self
 
 
 class Species(StrEnum):
@@ -11,44 +13,102 @@ class Species(StrEnum):
     ANCIENT = 'Ancient'
 
 
+class ProposalError(Exception):
+    pass
+
+
 @dataclass(frozen=True)
 class Player:
     species: Species
     hp: int
     lines: int
     ships: dict
+    request: dict
+    ship_costs = {'defender': 2}
 
     @classmethod
-    def new_player(cls, species):
+    def new_player(cls, species) -> Self:
         if species not in Species.__members__.values():
             raise ValueError(f"'{species} is not a valid species")
-        return cls(species, 25, 0, {})
+        return cls(species, 25, 0, {}, {})
 
-    def copy(self, species=None, hp=None, lines=None, ships=None):
+    def copy(self, species=None, hp=None, lines=None, ships=None, request=None) -> Self:
         return Player(
             self.species if species is None else species,
             self.hp if hp is None else hp,
             self.lines if lines is None else lines,
             self.ships if ships is None else ships,
+            self.request if request is None else request,
         )
 
-    def start_turn(self, die_roll):
+    def start_turn(self, die_roll) -> Self:
         return self.copy(lines=self.lines + die_roll)
 
-    def battle(self, other):
+    def battle(self, other) -> Self:
         return self.copy(hp=self.hp - 5)
 
-    def requests(self, phase):
-        return []
+    def generate_requests(self, phase) -> Self:
+        if phase == Phase.BUILD:
+            return self.copy(request=self.generate_possible_builds())
+        return self.copy(request={})
+
+    def generate_possible_builds(self) -> dict:
+        proposals = [{'ships': {}, 'lines': self.lines, 'seen': False}]
+        while any(not proposal['seen'] for proposal in proposals):
+            for proposal in proposals:
+                if proposal['seen']:
+                    continue
+                proposal['seen'] = True
+                for name, line_cost in Player.ship_costs.items():
+                    if proposal['lines'] >= line_cost:
+                        new_proposal = {
+                            'ships': deepcopy(proposal['ships']),
+                            'lines': proposal['lines'] - line_cost,
+                            'seen': False,
+                        }
+                        if name in proposal['ships'].keys():
+                            new_proposal['ships'][name] += 1
+                        else:
+                            new_proposal['ships'][name] = 1
+                        proposals.append(new_proposal)
+        return {'type': 'build', 'options': [proposal['ships'] for proposal in proposals]}
+
+    def clear_request(self):
+        return self.copy(request={})
+
+    def submit(self, proposal):
+        try:
+            if proposal['type'] != self.request['type']:
+                raise ProposalError()
+            if  proposal['option'] not in self.request['options']:
+                raise ProposalError()
+        except KeyError:
+            raise ProposalError()
+        if proposal['type'] == 'build':
+            return self.build(proposal['option'])
+        raise ProposalError()
+
+    def build(self, ships) -> Self:
+        remaining_lines = self.lines
+        for ship, quantity in ships.items():
+            remaining_lines -= quantity * Player.ship_costs[ship]
+            if remaining_lines < 0:
+                raise ProposalError()
+
+        return self.copy(lines=remaining_lines, ships={
+            ship: self.ships.get(ship, 0) + ships.get(ship, 0)
+            for ship in set(self.ships.keys()).union(set(ships.keys()))
+        })
+
 
 class Phase(Enum):
-    START_TURN = auto()
+    BUILD = auto()
     BATTLE = auto()
 
-    def next_phase(self):
+    def next_phase(self) -> Self:
         return {
-            self.START_TURN: Phase.BATTLE,
-            self.BATTLE: Phase.START_TURN,
+            self.BUILD: Phase.BATTLE,
+            self.BATTLE: Phase.BUILD,
         }[self]
 
 
@@ -58,36 +118,54 @@ class Game:
     players: list[Player]
 
     @classmethod
-    def start_game(cls, player_species):
-        return cls(Phase.START_TURN, [Player.new_player(species) for species in player_species])
+    def start_game(cls, player_species) -> Self:
+        return cls(Phase.BUILD, [
+            Player.new_player(species) for species in player_species
+        ]).resolve_phase().generate_requests()
 
-    def copy(self, phase=None, players=None):
+    def copy(self, phase=None, players=None) -> Self:
         return Game(
             self.phase if phase is None else phase,
             self.players if players is None else players,
         )
 
-    def next(self):
-        all_requests = [player.requests(self.phase) for player in self.players]
-        if any(len(requests) > 0 for requests in all_requests):
-            return self, all_requests
-        return self.resolve_phase().next_phase(), []
+    def next(self) -> tuple[Self, list]:
+        requests = [player.request for player in self.players]
+        if any(request != {} for request in requests):
+            return self, requests
+        return self.next_phase().resolve_phase().generate_requests(), []
 
-    def resolve_phase(self):
+    def generate_requests(self):
+        return self.copy(players=[player.generate_requests(self.phase) for player in self.players])
+
+    def submit(self, player_id, proposal):
+        if player_id >= len(self.players) or player_id < 0:
+            return self, False
+        try:
+            return self.copy(players=[
+                player.submit(proposal).clear_request() if i == player_id else player
+                for i, player in enumerate(self.players)
+            ]), True
+        except ProposalError:
+            return self, False
+
+    def resolve_phase(self) -> Self:
         match self.phase:
-            case Phase.START_TURN:
+            case Phase.BUILD:
                 return self.start_turn()
             case Phase.BATTLE:
                 return self.battle()
+            case _:
+                return self
 
-    def next_phase(self):
+    def next_phase(self) -> Self:
         return self.copy(phase=self.phase.next_phase())
 
-    def start_turn(self):
+    def start_turn(self) -> Self:
         die_roll = random.randint(1, 6)
         return self.copy(players=[player.start_turn(die_roll) for player in self.players])
 
-    def battle(self):
+    def battle(self) -> Self:
         return self.copy(players=[
             reduce(
                 lambda x, y: x.battle(y),
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,9 @@
 import random
 
-from shapeships_engine.main import *
+from shapeships_engine.main import Game, Species, Phase
 
 def test_start_game():
+    random.seed(0)
     game = Game.start_game([Species.HUMAN, Species.XENITE])
     players = game.players
     assert len(players) == 2
@@ -10,17 +11,51 @@ def test_start_game():
     assert players[1].species == Species.XENITE
     for player in players:
         assert player.hp == 25
-        assert player.lines == 0
+        assert player.lines == 4
         assert player.ships == {}
 
 def test_start_turn():
     random.seed(0)
     game = Game.start_game([Species.HUMAN, Species.HUMAN])
-    assert game.phase == Phase.START_TURN
+    assert game.phase == Phase.BUILD
     game, requests = game.next()
-    assert requests == []
+    assert game.phase == Phase.BUILD
     for player in game.players:
         assert player.lines == 4
+    assert requests == [
+        {'type': 'build', 'options': [{}, {'defender': 1}, {'defender': 2}]},
+        {'type': 'build', 'options': [{}, {'defender': 1}, {'defender': 2}]},
+    ]
+    game, success = game.submit(0, {'type': 'build', 'option': {'defender': 1}})
+    assert success is True
+    game, success = game.submit(1, {'type': 'build', 'option': {'defender': 2}})
+    assert success is True
+    assert game.players[0].ships['defender'] == 1
+    assert game.players[1].ships['defender'] == 2
+    game, requests = game.next()
+    assert game.phase == Phase.BATTLE
+    assert requests == []
+
+def test_build_failures():
+    random.seed(0)
+    game = Game.start_game([Species.HUMAN, Species.HUMAN])
+    assert game.phase == Phase.BUILD
+    game, requests = game.next()
+    assert game.phase == Phase.BUILD
+    for player in game.players:
+        assert player.lines == 4
+    assert requests == [
+        {'type': 'build', 'options': [{}, {'defender': 1}, {'defender': 2}]},
+        {'type': 'build', 'options': [{}, {'defender': 1}, {'defender': 2}]},
+    ]
+    game, success = game.submit(-5, {'type': 'build', 'option': {'defender': 1}})
+    assert success is False
+    game, success = game.submit(0, {'type': 'something_else', 'option': {'defender': 1}})
+    assert success is False
+    game, success = game.submit(0, {'type': 'build', 'option': {'defender': 1000}})
+    assert success is False
+    game, success = game.submit(0, {'type': 'build', 'option': {'defender': 1}})
+    assert success is True
 
 def test_battle():
     random.seed(0)
@@ -28,7 +63,8 @@ def test_battle():
     game, requests = game.next()
     for player in game.players:
         assert player.hp == 25
+    game, success = game.submit(0, {'type': 'build', 'option': {'defender': 1}})
+    game, success = game.submit(1, {'type': 'build', 'option': {'defender': 2}})
     game, requests = game.next()
-    assert requests == []
     for player in game.players:
         assert player.hp == 20
